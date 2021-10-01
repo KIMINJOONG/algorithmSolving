5abf2254f5bcfee30e6fd1f090aaaaff02362518
KIMINJOONG <ijboym216@gmail.com>
2021-10-01 20:44:24 +0900

    init

---
+++ src/com/company/FindString.java
@@ -0,0 +1,30 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class FindString {
+    public static int solution(String str, char t) {
+        int answer = 0;
+        str = str.toUpperCase();
+        t = Character.toUpperCase(t);
+
+        // 기존 for문을 이용한 방식
+//        for(int i = 0; i < str.length(); i++) {
+//            if(str.charAt(i) == t) {
+//                answer++;
+//            }
+//        }
+
+        //향상된 for문
+        for(char c : str.toCharArray()) {
+            if(c == t) answer++;
+        }
+        return answer;
+    }
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        String str = kb.next();
+        char c = kb.next().charAt(0);
+        System.out.println(solution(str, c));
+    }
+}
